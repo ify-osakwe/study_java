5da74555de132c442e6920f60dcb598de15afd20
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2023-06-28 15:16:08 +0100

    Add

---
+++ src/timBulchalka/intro/HelloWorld.java
@@ -0,0 +1,12 @@
+package timBulchalka.intro;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /* This is a statement in Java. A statement is a command to be executed.
+        *  It can include one or more expressions */
+        System.out.println("Hello World");
+
+        // In Java, String literals are always in double quotes (" ")
+        System.out.println("Hello Tim");
+    }
+}
