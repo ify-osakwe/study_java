ce0c99eead8bd1e1f0fd3e9f4f024cccc97a9a51
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2023-07-16 21:56:35 +0100

    add

--- src/timBulchalka/intro/Operators2.java
+++ src/timBulchalka/intro/Operators2.java
@@ -4,8 +4,12 @@ public class Operators2 {
     public static void main(String[] args) {
         // Ternary Operator
         // a short form "if (statement) this else that"
-        String makeOfCar = "Volkswagen";
+        String makeOfCar = modelCar();
         boolean isAVolks = makeOfCar.equals("Volswagen") ? false : true;
         System.out.println(isAVolks);
+    }
+
+    static String modelCar() {
+        return "Volks";
     }
 }
