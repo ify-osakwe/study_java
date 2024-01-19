9b6ad41d8ecb929bd1f098dec850f1b1487fce2c
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2024-01-19 21:54:21 +0100

    command pattern

---
+++ src/designPatterns/behavioural/command/CommandInterface.java
@@ -0,0 +1,44 @@
+package designPatterns.behavioural.command;
+
+public interface CommandInterface {
+    void execute();
+}
+
+class OpenFileCommand implements CommandInterface {
+    private FileSystemReceiver fileSystemReceiver;
+
+    public OpenFileCommand(FileSystemReceiver fileSystemReceiver) {
+        this.fileSystemReceiver = fileSystemReceiver;
+    }
+
+    @Override
+    public void execute() {
+        this.fileSystemReceiver.openFile();
+    }
+}
+
+class CloseFileCommand implements CommandInterface {
+    private FileSystemReceiver fileSystemReceiver;
+
+    public CloseFileCommand(FileSystemReceiver fileSystemReceiver) {
+        this.fileSystemReceiver = fileSystemReceiver;
+    }
+
+    @Override
+    public void execute() {
+        this.fileSystemReceiver.closeFile();
+    }
+}
+
+class WriteFileCommand implements CommandInterface {
+    private FileSystemReceiver fileSystemReceiver;
+
+    public WriteFileCommand(FileSystemReceiver fileSystemReceiver) {
+        this.fileSystemReceiver = fileSystemReceiver;
+    }
+
+    @Override
+    public void execute() {
+        this.fileSystemReceiver.writeFile();
+    }
+}
