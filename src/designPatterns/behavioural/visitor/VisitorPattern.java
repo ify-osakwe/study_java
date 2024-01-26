1f4c1ea15af822e134a7254f88f10584d83fd533
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2024-01-26 22:13:11 +0100

    visitor pattern

--- src/designPatterns/behavioural/visitor/VisitorPattern.java
+++ src/designPatterns/behavioural/visitor/VisitorPattern.java
@@ -1,7 +1,107 @@
 package designPatterns.behavioural.visitor;
 
+interface ItemElement {
+    int accept(ShoppingCartVisitor visitor);
+}
+
+class Book implements ItemElement {
+    private int price;
+    private String isbnNumber;
+
+    public Book(int price, String isbnNumber) {
+        this.price = price;
+        this.isbnNumber = isbnNumber;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getIsbnNumber() {
+        return isbnNumber;
+    }
+
+    @Override
+    public int accept(ShoppingCartVisitor visitor) {
+        return visitor.visit(this);
+    }
+}
+
+class Fruit implements ItemElement {
+    private int pricePerKg;
+    private int weight;
+    private String name;
+
+    public Fruit(int pricePerKg, int weight, String name) {
+        this.pricePerKg = pricePerKg;
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public int getPricePerKg() {
+        return pricePerKg;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int accept(ShoppingCartVisitor visitor) {
+        return visitor.visit(this);
+    }
+}
+
+interface ShoppingCartVisitor {
+    int visit(Book book);
+
+    int visit(Fruit fruit);
+}
+
+class ShoppingCartVisitorImpl implements ShoppingCartVisitor {
+
+    @Override
+    public int visit(Book book) {
+        int cost;
+        if (book.getPrice() > 50) {
+            cost = book.getPrice() - 5;
+        } else {
+            cost = book.getPrice();
+        }
+        System.out.println("Book ISBN::" + book.getIsbnNumber() + " cost =" + cost);
+        return cost;
+    }
+
+    @Override
+    public int visit(Fruit fruit) {
+        int cost = fruit.getPricePerKg() * fruit.getWeight();
+        System.out.println(fruit.getName() + " cost = " + cost);
+        return cost;
+    }
+}
+
 public class VisitorPattern {
     public static void main(String[] args) {
+        ItemElement[] items = new ItemElement[]{
+                new Book(20, "1234"),
+                new Book(100, "5678"),
+                new Fruit(10, 2, "Banana"),
+                new Fruit(5, 5, "Apple")};
 
+        int total = calculatePrice(items);
+        System.out.println("Total Cost = " + total);
+    }
+
+    private static int calculatePrice(ItemElement[] items) {
+        ShoppingCartVisitor visitor = new ShoppingCartVisitorImpl();
+        int sum = 0;
+        for (ItemElement item : items) {
+            sum = sum + item.accept(visitor);
+        }
+        return sum;
     }
 }
