cb778f5aa25f685f9c5a79273de06cbe024378b7
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2024-05-15 15:50:09 +0100

    add. 15-may-2024

---
+++ src/timBulchalka/codingExercise/Exercise1.java
@@ -0,0 +1,33 @@
+package timBulchalka.codingExercise;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        checkNumber(54);
+        checkNumber(-2);
+        checkNumber(18);
+        checkNumber(3);
+        checkNumber(0);
+        checkNumber(-32);
+        checkNumber(7);
+
+    }
+
+    public static void checkNumber(int number) {
+//        if (number == 0) {
+//            System.out.println("zero");
+//        }
+//        if (number < 0) {
+//            System.out.println("negative");
+//        }
+//        if (number > 0) {
+//            System.out.println("positive");
+//        }
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
---
+++ src/timBulchalka/javaDataStructures/collections/CardDeck.java
@@ -0,0 +1,190 @@
+package timBulchalka.javaDataStructures.collections;
+
+import java.util.*;
+
+public class CardDeck {
+    public static void main(String[] args) {
+        List<Card> deck1 = Card.getStandardDeck();
+        Card.printDeck(deck1);
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+
+        Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards, "Card Collection with Aces added", 2);
+
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards, "Card Collection with Kings copied", 2);
+
+        cards = List.copyOf(kingsOfClubs);
+        Card.printDeck(cards, "List Copy of Kings", 1);
+
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+
+        Collections.shuffle(deck);
+        Card.printDeck(deck, "Shuffled Deck", 4);
+
+        Collections.reverse(deck);
+        Card.printDeck(deck, "Reversed Deck of Cards:", 4);
+
+        var sortingAlgorithm = Comparator.comparing(Card::rank)
+                .thenComparing(Card::suit);
+        Collections.sort(deck, sortingAlgorithm);
+        Card.printDeck(deck, "Standard Deck sorted by rank, suit", 13);
+
+        Collections.reverse(deck);
+        Card.printDeck(deck, "Sorted by rank, suit reversed:", 13);
+
+        List<Card> kings = new ArrayList<>(deck.subList(4, 8));
+        Card.printDeck(kings, "Kings in deck", 1);
+
+        List<Card> tens = new ArrayList<>(deck.subList(16, 20));
+        Card.printDeck(tens, "Tens in deck", 1);
+
+        int subListIndex = Collections.indexOfSubList(deck, tens);
+        System.out.println("sublist index for tens = " + subListIndex);
+        System.out.println("Contains = " + deck.containsAll(tens));
+
+        boolean disjoint = Collections.disjoint(deck, tens);
+        System.out.println("disjoint = " + disjoint);
+
+        boolean disjoint2 = Collections.disjoint(kings, tens);
+        System.out.println("disjoint = " + disjoint2);
+
+        deck.sort(sortingAlgorithm);
+        Card tenOfHearts = Card.getNumericCard(Card.Suit.HEART, 10);
+        int foundIndex = Collections.binarySearch(deck, tenOfHearts, sortingAlgorithm);
+        System.out.println("foundIndex = " + foundIndex);
+        System.out.println("foundIndex = " + deck.indexOf(tenOfHearts));
+        System.out.println(deck.get(foundIndex));
+
+        Card tenOfClubs = Card.getNumericCard(Card.Suit.CLUB, 10);
+        Collections.replaceAll(deck, tenOfClubs, tenOfHearts);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        Collections.replaceAll(deck, tenOfHearts, tenOfClubs);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        if (Collections.replaceAll(deck, tenOfHearts, tenOfClubs)) {
+            System.out.println("Tens of hearts replaced with tens of clubs");
+        } else {
+            System.out.println("No tens of hearts found in the list");
+        }
+
+        System.out.println("Ten of Clubs Cards = " +
+                Collections.frequency(deck, tenOfClubs));
+
+        System.out.println("Best Card = " + Collections.max(deck, sortingAlgorithm));
+        System.out.println("Worst Card = " + Collections.min(deck, sortingAlgorithm));
+
+        var sortBySuit = Comparator.comparing(Card::suit)
+                .thenComparing(Card::rank);
+        deck.sort(sortBySuit);
+        Card.printDeck(deck, "Sorted by Suit, Rank", 4);
+
+        List<Card> copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.rotate(copied, 2);
+        System.out.println("UnRotated: " + deck.subList(0, 13));
+        System.out.println("Rotated " + 2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.rotate(copied, -2);
+        System.out.println("UnRotated: " + deck.subList(0, 13));
+        System.out.println("Rotated " + -2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0, 13));
+        for (int i = 0; i < copied.size() / 2; i++) {
+            Collections.swap(copied, i, copied.size() - 1 - i);
+        }
+        System.out.println("Manual reverse :" + copied);
+
+        copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.reverse(copied);
+        System.out.println("Using reverse :" + copied);
+
+    }
+}
+
+record Card(Suit suit, String face, int rank) {
+
+    public enum Suit {
+        CLUB, DIAMOND, HEART, SPADE;
+
+        public char getImage() {
+            return (new char[]{9827, 9830, 9829, 9824})[this.ordinal()];
+        }
+    }
+
+    @Override
+    public String toString() {
+        int index = face.equals("10") ? 2 : 1;
+        String faceString = face.substring(0, index);
+        return "%s%c(%d)".formatted(faceString, suit.getImage(), rank);
+    }
+
+    public static Card getNumericCard(Suit suit, int cardNumber) {
+        if (cardNumber > 1 && cardNumber < 11) {
+            return new Card(suit, String.valueOf(cardNumber), cardNumber - 2);
+        }
+        System.out.println("Invalid Numeric card selected");
+        return null;
+    }
+
+    public static Card getFaceCard(Suit suit, char abbrev) {
+        int charIndex = "JQKA".indexOf(abbrev);
+        if (charIndex > -1) {
+            return new Card(suit, "" + abbrev, charIndex + 9);
+        }
+        System.out.println("Invalid Face card selected");
+        return null;
+    }
+
+    public static List<Card> getStandardDeck() {
+        List<Card> deck = new ArrayList<>(52);
+        for (Suit suit : Suit.values()) {
+            for (int i = 2; i <= 10; i++) {
+                deck.add(getNumericCard(suit, i));
+            }
+            for (char c : new char[]{'J', 'Q', 'K', 'A'}) {
+                deck.add(getFaceCard(suit, c));
+            }
+        }
+        return deck;
+    }
+
+    public static void printDeck(List<Card> deck) {
+        printDeck(deck, "Current Deck", 4);
+    }
+
+    public static void printDeck(List<Card> deck, String description, int rows) {
+        System.out.println("---------------------------");
+        if (description != null) {
+            System.out.println(description);
+        }
+        int cardsInRow = deck.size() / rows;
+        for (int i = 0; i < rows; i++) {
+            int startIndex = i * cardsInRow;
+            int endIndex = startIndex + cardsInRow;
+            deck.subList(startIndex, endIndex).forEach(c -> System.out.print(c + " "));
+            System.out.println();
+        }
+    }
+
+}
+
