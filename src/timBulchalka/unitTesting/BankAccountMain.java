201368f79ab3c1605de95cc65799db19ff339392
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2024-05-17 21:52:36 +0100

    add 17-May-2024

---
+++ src/timBulchalka/unitTesting/BankAccountMain.java
@@ -0,0 +1,7 @@
+package timBulchalka.unitTesting;
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+        System.out.println("The application is running");
+    }
+}
---
+++ src/timBulchalka/unitTesting/UtilitiesMain.java
@@ -0,0 +1,8 @@
+package timBulchalka.unitTesting;
+
+public class UtilitiesMain {
+    public static void main(String[] args) {
+        Utilities util = new Utilities();
+        util.removePairs("AABCDDEFF");
+    }
+}
